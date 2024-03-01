9d6ffb501729fe91239589adb2062dfc193620ca
Chris Openshaw <copenshaw17@gmail.com>
2024-03-01 14:26:52 -0600

    Redo

--- qb.go
+++ qb.go
@@ -1,57 +1,33 @@
 package mssqlqb
 
-import (
-	"bytes"
-	"errors"
-	"fmt"
-)
+type statement string
+
+const SELECT statement = "SELECT"
 
 type Query struct {
-	bSelect bool
-	Bytes   bytes.Buffer
+	stateType statement
+	text      []byte
+	isValid   bool
+}
+
+type Table struct {
+	name []byte
 }
 
 type IQuery interface {
-	Read()
-	Select()
 	From()
 }
 
-func NewQuery() Query {
+func NewQuery(s statement) Query {
 	return Query{
-		bSelect: false,
+		stateType: SELECT,
+		text:      []byte("SELECT"),
+		isValid:   false,
 	}
 }
 
-func (q *Query) String() string {
-	return q.Bytes.String()
-}
-
-func (q *Query) Select(columns []string) error {
-	if len(columns) == 0 {
-		err := errors.New("cannot accept empty string slice")
-		return err
+func (q *Query) From(columns [][]byte, table []byte) {
+	if q.stateType == SELECT {
+		q.text
 	}
-	q.bSelect = true
-	q.Bytes.Write([]byte("SELECT "))
-
-	for key, value := range columns {
-		if key == len(columns)-1 {
-			q.Bytes.Write([]byte(value))
-		} else {
-			q.Bytes.Write([]byte(value + ", "))
-		}
-	}
-	return nil
-}
-
-func (q *Query) From(table string, alias string) error {
-	if !q.bSelect {
-		err := errors.New("no select statement initialized")
-		return err
-	}
-	if 
-	fString := fmt.Sprintf(" FROM %s", table)
-	q.Bytes.Write([]byte(fString))
-	return nil
 }
--- qb_test.go
+++ qb_test.go
@@ -1,56 +1,11 @@
 package mssqlqb
 
 import (
+	"fmt"
 	"testing"
 )
 
-var (
-	testCols  = []string{"Name", "Id", "Password"}
-	testTable = "Users"
-)
-
-func TestSelect(t *testing.T) {
-	t.Run("Select Test", func(t *testing.T) {
-		myquery := NewQuery()
-		err := myquery.Select(testCols)
-		if err != nil {
-			t.Error("Error thrown for TestSelect")
-		}
-		t.Log(myquery.String())
-	})
-}
-
-func BenchmarkSelectAndRead(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		myquery := NewQuery()
-		err := myquery.Select(testCols)
-		if err != nil {
-			b.Error("Error thrown for TestSelect")
-		}
-		myquery.String()
-	}
-}
-
-func TestSelectError(t *testing.T) {
-	t.Run("Select Error", func(t *testing.T) {
-		myquery := NewQuery()
-		var columns []string
-		err := myquery.Select(columns)
-		if err == nil {
-			t.Error("Error thrown for TestSelect")
-		}
-		t.Log(err)
-		t.Log(myquery.String())
-	})
-}
-
-func TestFromEmptySelect(t *testing.T) {
-	t.Run("Append from to empty select", func(t *testing.T) {
-		myquery := NewQuery()
-		err := myquery.From(testTable, "")
-		if err == nil {
-			t.Error("Error thrown for TestFromEmptySelect")
-		}
-		t.Log(err)
-	})
+func TestXxx(t *testing.T) {
+	q := NewQuery(SELECT)
+	fmt.Print(q.stateType)
 }
